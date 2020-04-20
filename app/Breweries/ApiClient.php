8d41f017d54506240751bfbe4497ef9844b0a282
Michael Peacock <mkpeacock@gmail.com>
2020-04-20 16:10:00 +0100

    Add support for pagination. A bit hacky because we can't lookup total records, but the data is available (https://github.com/chrisjm/openbrewerydb-rails-api/blob/master/db/breweries.csv) which gives us a value to hard code.

--- app/Breweries/ApiClient.php
+++ app/Breweries/ApiClient.php
@@ -3,6 +3,7 @@
 namespace App\Breweries;
 
 use GuzzleHttp\Client;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 class ApiClient
@@ -17,18 +18,28 @@ class ApiClient
 
     public function getPaginated(int $page = 1)
     {
+        $perPage = 20;
+
         $breweryIndexData = $this->httpClient->get(
             $this->baseUrl . '/breweries',
             [
-               'query' => ['page' => $page]
+               'query' => [
+                   'page' => $page,
+                   'per_page' => $perPage
+               ]
             ]
         )->getBody()->getContents();
 
-        $breweryIndexData = new Collection(json_decode($breweryIndexData, true));
 
-        return $breweryIndexData->map(function (array $breweryData) {
-            return $this->populateBrewery($breweryData);
-        });
+        // Sadly, the regular paginator won't give a next link unless it is given the entire collection.
+        // The LengthAwarePagination requires the total length of the dataset, which we don't know programatically.
+        // However, the data source for this indicates the length is 8027 so we will use that to illustrate the feature
+        return (new Collection(json_decode($breweryIndexData, true)))
+            ->map(function (array $breweryData) {
+                return $this->populateBrewery($breweryData);
+            })->pipe(function (Collection $collection) use ($page, $perPage) {
+                return new LengthAwarePaginator($collection, 8027, $perPage, $page, ['path' => '/api/breweries']);
+            });
     }
 
     public function getBrewery(int $breweryId)
